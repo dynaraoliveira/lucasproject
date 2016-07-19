b7afcaa1922d629ece5f4aa31c56e1fe0435ca99
Dynara Rico Oliveira <dynara.oliveira@gmail.com>
2016-07-19 03:28:00 +0000

    alteracao config

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -33,7 +33,7 @@ class SensorsController < ApplicationController
     @s5 = params[:s5]
     
     
-    @sensor = Sensor.create(chave: @chave, sensor0: @as0, sensor1: @as1, sensor2: @as2, sensor3: @as3, sensor4: @as4, sensor5: @as5)
+    @sensor = Sensor.create(chave: @chave, sensor0: @s0, sensor1: @s1, sensor2: @s2, sensor3: @s3, sensor4: @s4, sensor5: @s5)
   
     respond_to do |format|
       if @sensor
